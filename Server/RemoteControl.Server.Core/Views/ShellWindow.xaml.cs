d09f776cee27a84d4a37b398fb5ff1fef036d1a1
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-11-03 23:29:00 +0100

    dev

--- Server/RemoteControl.Server.Core/Views/ShellWindow.xaml.cs
+++ Server/RemoteControl.Server.Core/Views/ShellWindow.xaml.cs
@@ -1,17 +1,44 @@
 ﻿using JToolbox.WPF.UI;
 using MahApps.Metro.Controls;
+using System;
+using System.Windows;
 
 namespace RemoteControl.Server.Core.Views
 {
     public partial class ShellWindow : MetroWindow
     {
         private readonly WindowEvents windowEvents;
+        private WindowState windowState;
 
         public ShellWindow()
         {
             InitializeComponent();
             windowEvents = new WindowEvents(this);
             windowEvents.Attach();
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide();
+            }
+            else
+            {
+                windowState = WindowState;
+            }
+            base.OnStateChanged(e);
+        }
+
+        private void miRestore_Click(object sender, RoutedEventArgs e)
+        {
+            Show();
+            WindowState = windowState;
+        }
+
+        private void miClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
